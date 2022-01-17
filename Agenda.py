b5fdcead44c47690741afd95a6ae233fbcfd1e0b
andersonid <nobre.it@gmail.com>
2022-01-17 19:06:12 -0300

    Metodo mostrar_contatos criado.

--- Agenda.py
+++ Agenda.py
@@ -1,3 +1,24 @@
 AGENDA = {}
 
-nome = "Anderson"
+AGENDA['Anderson'] = {
+    'telefone': '11-970355726',
+    'email': 'nobre.it@gmail.com',
+    'endereco': 'Rua Taquacetuba, 201',
+}
+
+AGENDA['Graziela'] = {
+    'telefone': '11-972204544',
+    'email': 'graziela.claro@gmail.com',
+    'endereco': 'Rua Taquacetuba, 171',
+}
+
+def mostrar_contatos():
+    for contato in AGENDA:
+        print("Nome:", contato)
+        print("Telefone:", AGENDA[contato]['telefone'])
+        print("E-Mail:", AGENDA[contato]['email'])
+        print("Endereco:", AGENDA[contato]['endereco'])
+        print("______________")
+
+
+mostrar_contatos()
