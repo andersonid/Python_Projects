5027bad44442815275c80c59fd21c882dcedef6d
andersonid <nobre.it@gmail.com>
2022-02-02 18:06:54 -0300

    Agenda v3 Fixing files versions

--- Agenda.py
+++ Agenda.py
@@ -1,46 +1,53 @@
-AGENDA = {}
+import os
 
-AGENDA['Anderson'] = {
-    'telefone': '11-97035-5726',
+LEADS = {}
+
+LEADS['Anderson'] = {
+    'telefone': '11-970355726',
     'email': 'nobre.it@gmail.com',
-    'endereco': 'Rua Taquacetuba, 201',
 }
 
-AGENDA['Graziela'] = {
-    'telefone': '11-97220-4544',
+LEADS['Graziela'] = {
+    'telefone': '11-972204544',
     'email': 'graziela.claro@gmail.com',
-    'endereco': 'Rua Taquacetuba, 171',
 }
 
-def mostrar_contatos():
-    for contato in AGENDA:
-        buscar_contato(contato)
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
 
-def buscar_contato(contato):
-    print('_____________________________________________')
-    print('Nome:', contato)
-    print('Telefone:', AGENDA[contato]['telefone'])
-    print('E-Mail:', AGENDA[contato]['email'])
-    print('Endereco:', AGENDA[contato]['endereco'])
-    print('_____________________________________________')
-    print('')
+def mostrar_todos():
+    print('===================================')
+    print('     MOSTRANDO TODOS OS LEADS ')
+    print('===================================')
+
+    for lead in LEADS:
+        print(' Nome:', lead)
+        print(' Telefone:', LEADS[lead]['telefone'])
+        print(' E-mail:', LEADS[lead]['email'])
+        print('===================================')
 
 
-def incluir_contato(contato, telefone, email, endereco):
-    AGENDA[contato] = {
+def buscar_lead(lead):
+    clearConsole()
+    print('===================================')
+    print('    MOSTRANDO LEAD:', lead)
+    print('===================================')
+    print(' Telefone:', LEADS[lead]['telefone'])
+    print(' E-mail:', LEADS[lead]['email'])
+    print('===================================')
+    buscar_lead(input('Digite o nome a pesquisar: '))
+
+def add_lead(lead, telefone, email):
+    LEADS[lead] = {
         'telefone': telefone,
         'email': email,
-        'endereco': endereco,
     }
-    print('###################################################')
-    print('Contato {} adicionado com sucesso!'.format(contato))
-    print('###################################################')
-    print('')
 
 
-print('')
-mostrar_contatos()
-print('')
-incluir_contato('Joao', '11-96575-4568', 'jao@zica.co', 'Rua dos Bobos, 0')
-buscar_contato('Joao')
+buscar_lead(input('Digite o nome a pesquisar: '))
+# mostrar_todos()
